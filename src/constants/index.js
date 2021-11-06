768cc063a84c5a43a54d14d46bb404b765ae5785
DatNguyen <duydat10122001@gmail.com>
2021-11-06 09:57:29 +0700

    fix refreshpage

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,6 @@ export const USER_INFOR = "user";
 export const API_KEY = "api_key=5761f00d4efd80b92ba2496773204780";
 export const BASE_URL = "https://api.themoviedb.org/3";
 export const TOKEN = "app_token";
-// export const API_URL = BASE_URL + API_KEY;
 //CALLAPIALL:  api.themoviedb.org/3/trending/movie/week?api_key=5761f00d4efd80b92ba2496773204780&language=vi";
 
 
